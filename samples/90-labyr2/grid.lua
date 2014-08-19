9bd52c1e12a9e05477e8563df13bceda4cf848e7
gogoprog <gogoprog@gmail.com>
2014-08-19 12:30:08 +0200

    wip

--- samples/90-labyr2/grid.lua
+++ samples/90-labyr2/grid.lua
@@ -1,4 +1,8 @@
-Grid = Grid or {}
+dofile("component_placer.lua")
+
+Grid = Grid or {
+    tiles = {}
+}
 
 function Grid:init(w, h, tileSize)
     self.width = w
@@ -9,6 +13,8 @@ function Grid:init(w, h, tileSize)
         tileSize * ( w - 1 ) * -0.5,
         tileSize * ( h - 1 ) * -0.5
         }
+
+    self:initPlacers()
 end
 
 function Grid:getTilePosition(i, j)
@@ -33,12 +39,94 @@ function Grid:fill()
                 "sprite"
                 )
 
-            local x, y = self:getTilePosition(i, j)
-
-            e.position.x = x
-            e.position.y = y
+            self:setTile(i, j, e)
 
             e:insert()
         end
+    end
+end
+
+function Grid:setTile(i, j, e)
+    if self.tiles[i] == nil then
+        self.tiles[i] = {}
+    end
+
+    self.tiles[i][j] = e
+
+    local x, y = self:getTilePosition(i, j)
+
+    e.position.x = x
+    e.position.y = y
+end
+
+
+function Grid:createPlacer(i, j)
+    local e
+    e = entity.create()
+
+    e:addComponent(
+        ComponentSprite(),
+        {
+            texture = graphics.texture.get("tile0"),
+            extent = { x=self.tileSize, y=self.tileSize },
+            layer = 0
+        },
+        "sprite"
+        )
+
+    e:addComponent(
+        ComponentPlacer(),
+        {
+        },
+        "placer"
+        )
+    
+    e:addComponent(
+        ComponentMouseable(),
+        {
+            extent = { x=self.tileSize, y=self.tileSize }
+        }
+        )
+
+    e:insert()
+
+    local x, y = self:getTilePosition(i, j)
+
+    e.position.x = x
+    e.position.y = y
+
+    return e
+end
+
+function Grid:initPlacers()
+    local w = self.width -1
+    local h = self.height -1
+
+    local i = -1
+    for j=0,h do
+        local e = self:createPlacer(i, j)
+        e.placer.row = j
+        e.placer.sens = 1
+    end
+
+    i = w + 1
+    for j=0,h do
+        local e = self:createPlacer(i, j)
+        e.placer.row = j
+        e.placer.sens = -1
+    end
+
+    local j = -1
+    for i=0,w do
+        local e = self:createPlacer(i, j)
+        e.placer.col = i
+        e.placer.sens = 1
+    end
+
+    j = h + 1
+    for i=0,w do
+        local e = self:createPlacer(i, j)
+        e.placer.col = i
+        e.placer.sens = -1
     end
 end
