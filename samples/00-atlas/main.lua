b2ecdf5ad6833cdb40e8fbf2eb526b600f37c1ea
gogoprog <gogoprog@gmail.com>
2014-10-26 18:37:28 +0100

    Updated atlas sample

--- samples/00-atlas/main.lua
+++ samples/00-atlas/main.lua
@@ -1,30 +1,55 @@
 function init()
-    gengine.application.setName("[gengine-tests] 01-sprite")
+    gengine.application.setName("[gengine-tests] 00-atlas")
     gengine.application.setExtent(320,200)
 end
 
-local logoEntity
+local entity, entity2
 
 function start()
     gengine.graphics.setClearColor(0,0.1,0.1,1)
 
-    local texture = gengine.graphics.texture.create("logo.png")
+    local texture = gengine.graphics.texture.create("logo.png") -- Create a new texture.
 
-    gengine.graphics.atlas.create("test", texture, 5, 1)
+    gengine.graphics.atlas.create("atlas1", texture, 2, 1) -- Create a new atlas using the given texture that will be divided in 5x1 cells.
 
-    logoEntity = gengine.entity.create()
+    gengine.graphics.atlas.create( -- Create another atlas.
+        "atlas2",
+        texture,
+        {
+            vector4(10, 0, 64, 32),  -- Specify custom cells.
+            vector4(74, 0, 64, 32)   -- (x, y, width, height)
+        }
+        )
 
-    logoEntity:addComponent(
+
+    entity = gengine.entity.create()
+    entity2 = gengine.entity.create()
+
+    entity:addComponent(
         ComponentSprite(),
         {
-            atlas = gengine.graphics.atlas.get("test"),
-            atlasItem = 4,
-            extent = vector2(256, 128),
+            atlas = gengine.graphics.atlas.get("atlas1"), -- Will use the given atlas.
+            atlasItem = 1, -- Using the cell at index 0. (index starts from 0)
+            extent = vector2(128, 128),
             layer = 0
         }
         )
 
-    logoEntity:insert()
+    entity.position.x = - 80
+    entity:insert()
+
+    entity2:addComponent(
+        ComponentSprite(),
+        {
+            atlas = gengine.graphics.atlas.get("atlas2"), -- Will use the given atlas.
+            atlasItem = 0, -- Using the cell at index 0.
+            extent = vector2(128, 128),
+            layer = 0
+        }
+        )
+
+    entity2.position.x = 80
+    entity2:insert()
 end
 
 function update(dt)
@@ -34,5 +59,4 @@ function update(dt)
 end
 
 function stop()
-
 end
