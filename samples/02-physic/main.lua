c5df082ff086c65f2fe11cad0af81648d5f46cfd
gogoprog <gogoprog@gmail.com>
2014-10-30 23:00:44 +0100

    Wip on physic sample

--- samples/02-physic/main.lua
+++ samples/02-physic/main.lua
@@ -3,40 +3,51 @@ function init()
     gengine.application.setExtent(640, 480)
 end
 
-local logoEntity
+local cameraEntity
 
 function start()
     gengine.graphics.setClearColor(0,0.1,0.1,1)
 
-    gengine.graphics.texture.create("logo.png")
+    gengine.graphics.texture.create("tile.png")
 
-    logoEntity = gengine.entity.create()
+    cameraEntity = gengine.entity.create()
+    cameraEntity:addComponent(ComponentCamera(), { extent = vector2(640, 480) }, "camera")
+    cameraEntity:insert()
 
-    logoEntity:addComponent(
+    local e = gengine.entity.create()
+
+    e:addComponent(
         ComponentSprite(),
         {
-            texture = gengine.graphics.texture.get("logo"),
-            extent = vector2(256, 128),
-            layer = 0
+            texture = gengine.graphics.texture.get("tile"),
+            color = vector4(0.5, 0.2, 0.2, 1),
+            extent = vector2(640, 640),
+            uvScale = vector2(20, 20),
+            layer = -1
         }
         )
 
-    logoEntity:addComponent(
+    e:addComponent(
         ComponentPhysic(),
         {
-            extent = vector2(256, 128),
-            type = "dynamic"
+            extent = vector2(640, 640),
+            type = "static"
         }
         )
 
-    logoEntity.position:set(0,200)
-    logoEntity:insert()
+    e.position:set(0,-500)
+    e:insert()
+
+    createBloc(0, 200)
 end
 
-local total = 0
-
 function update(dt)
-    total = total + dt
+    if gengine.input.mouse:isDown(1) then
+        local x,y = gengine.input.mouse:getPosition()
+        local wx, wy = cameraEntity.camera:getWorldPosition(x,y)
+
+        createBloc(wx, wy)
+    end
 
     if gengine.input.keyboard:isJustUp(27) then
         gengine.application.quit()
@@ -45,4 +56,32 @@ end
 
 function stop()
 
+end
+
+function createBloc(x, y)
+    local e = gengine.entity.create()
+
+    e:addComponent(
+        ComponentSprite(),
+        {
+            texture = gengine.graphics.texture.get("tile"),
+            extent = vector2(16, 16),
+            layer = 0
+        }
+        )
+
+    e:addComponent(
+        ComponentPhysic(),
+        {
+            extent = vector2(16, 16),
+            type = "dynamic",
+            density = 1,
+            friction = 1.3
+        }
+        )
+
+    e.position:set(x, y)
+    e:insert()
+
+    return e
 end
