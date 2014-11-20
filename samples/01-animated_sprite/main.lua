8d4a766e4a3341f4b7bc3efe26e0ba23b347303e
Gauthier Billot <gauthier.billot@fishingcactus.com>
2014-11-20 12:51:56 +0100

    Updated animated sprite sample

--- samples/01-animated_sprite/main.lua
+++ samples/01-animated_sprite/main.lua
@@ -3,7 +3,8 @@ function init()
     gengine.application.setExtent(320,200)
 end
 
-local logoEntity
+local characterEntity
+local leftAnimation, rightAnimation, loopingAnimation
 
 function start()
     gengine.graphics.setClearColor(0,0.1,0.1,1)
@@ -12,29 +13,60 @@ function start()
 
     local atlas = gengine.graphics.atlas.create("test", texture, 12, 8)
 
-    local animation = gengine.graphics.animation.create("testa",
+    loopingAnimation = gengine.graphics.animation.create(
+        "looping_animation",
         {
             atlas = atlas,
             frames = { 0, 1, 2, 1 },
-            framerate = 5
+            framerate = 5,
+            loop = true
         }
         )
 
-    logoEntity = gengine.entity.create()
+    leftAnimation = gengine.graphics.animation.create(
+        "left",
+        {
+            atlas = atlas,
+            frames = { 12, 13, 14, 13 },
+            framerate = 5,
+            loop = false
+        }
+        )
 
-    logoEntity:addComponent(
+    rightAnimation = gengine.graphics.animation.create(
+        "right",
+        {
+            atlas = atlas,
+            frames = { 24, 25, 26, 25 },
+            framerate = 5,
+            loop = false
+        }
+        )
+
+    characterEntity = gengine.entity.create()
+
+    characterEntity:addComponent(
         ComponentAnimatedSprite(),
         {
-            animation = animation,
-            extent =vector2(128, 128),
+            animation = loopingAnimation,
+            extent = vector2(128, 128),
             layer = 0
-        }
+        },
+        "animatedSprite"
         )
 
-    logoEntity:insert()
+    characterEntity:insert()
 end
 
 function update(dt)
+    if gengine.input.keyboard:isJustUp(80) then -- left arrow
+        characterEntity.animatedSprite:pushAnimation(leftAnimation)
+    end
+
+    if gengine.input.keyboard:isJustUp(79) then -- right arrow
+        characterEntity.animatedSprite:pushAnimation(rightAnimation)
+    end
+
     if gengine.input.keyboard:isJustUp(41) then
         gengine.application.quit()
     end
