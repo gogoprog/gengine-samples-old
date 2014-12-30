f96d8a95422cfbb3d1ed3e7acafa5306dbb59286
gogoprog <gogoprog@gmail.com>
2014-12-30 23:03:30 +0100

    Updated isometric sample

--- samples/80-isometric/main.lua
+++ samples/80-isometric/main.lua
@@ -1,26 +1,25 @@
-function init()
-    gengine.application.setName("[gengine-tests] 80-isometric")
-    gengine.application.setExtent(640,480)
-end
 
-local entity
+IsometricMap = {
+    cellSize = 32
+}
 
-function start()
-    gengine.graphics.setClearColor(0,0.1,0.1,1)
-
+function IsometricMap:init()
     local texture = gengine.graphics.texture.create("tile_set.png") -- Loading the sprite sheet.
 
     gengine.graphics.atlas.create(
         "tile_set",
         texture,
         { 
-            vector4(0, 0, 64, 64), -- Grass
-            vector4(1*64, 0*64, 64, 64), -- Tree
-            vector4(4*64, 5*64, 64, 64), -- Building
+            -- Grass
+            vector4(0*64, 0*64, 64, 64),
+            vector4(1*64, 0*64, 64, 64),
+            vector4(2*64, 0*64, 64, 64),
+            vector4(3*64, 0*64, 64, 64),
+            vector4(4*64, 0*64, 64, 64),
         }
         )
 
-    entity = gengine.entity.create()
+    local entity = gengine.entity.create()
 
     entity:addComponent(
         ComponentSpriteBatch(),
@@ -37,23 +36,46 @@ function start()
     batch:lock()
 
     -- Fill the screen with grass
-    for j=-240,240,32 do
-        for i=-320,320,32 do
-            batch:addItem(0, vector2(i, j))
+    for j=-240,240,self.cellSize do
+        for i=-320,320,self.cellSize do
+            batch:addItem(math.random(0, 4), self:getIsoFromCar(i, j))
         end
     end
-
-    -- Put trees at random positions
-    for i=1,10 do
-        batch:addItem(1, vector2(math.random(-10,10) * 32, math.random(-10,10) * 32))
-    end
-
-    -- Put a building at random position
-    batch:addItem(2, vector2(0, 0))
 
     batch:unlock()
 
     entity:insert()
+end
+
+function IsometricMap:getIsoFromCar(x, y)
+    return vector2(x - y, (x + y) / 2)
+end
+
+function IsometricMap:getCarFromIso(i, j)
+    return vector2((i + 2*j) /2, (2*j - i )/2)
+end
+
+local cameraEntity, lastMouseX, lastMouseY
+
+function init()
+    gengine.application.setName("[gengine-tests] 80-isometric")
+    gengine.application.setExtent(640,480)
+end
+
+function start()
+    gengine.graphics.setClearColor(0,0.1,0.1,1)
+
+    IsometricMap:init()
+
+    cameraEntity = gengine.entity.create()
+    cameraEntity:addComponent(
+        ComponentCamera(),
+        { 
+            extent = vector2(640, 480)
+        },
+        "camera"
+        )
+    cameraEntity:insert()
 end
 
 
@@ -61,6 +83,18 @@ function update(dt)
     if gengine.input.keyboard:isJustUp(41) then
         gengine.application.quit()
     end
+
+    local mx, my = gengine.input.mouse:getPosition()
+
+    if gengine.input.mouse:isDown(3) then
+        local dx, dy = lastMouseX - mx, lastMouseY - my
+
+        cameraEntity.position.x = cameraEntity.position.x + dx
+        cameraEntity.position.y = cameraEntity.position.y - dy
+    end
+
+    lastMouseX = mx
+    lastMouseY = my
 end
 
 function stop()
@@ -68,10 +102,3 @@ function stop()
 end
 
 
-function CarToIso(x, y)
-    return x - y, (x + y) / 2
-end
-
-function IsoToCar(i, j)
-    return (i + 2*j) /2, (2*j - i )/2
-end
