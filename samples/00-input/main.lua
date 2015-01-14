1fee796b579925082a2fbb7d9457c393eea9a55a
Gauthier Billot <gauthier.billot@fishingcactus.com>
2015-01-14 13:38:59 +0100

    Updated input sample

--- samples/00-input/main.lua
+++ samples/00-input/main.lua
@@ -3,20 +3,38 @@ function init()
     gengine.application.setExtent(320,200)
 end
 
-local connectedJoypads = {}
+local lastMousePosition
 
 function start()
-
+    lastMousePosition = vector2()
 end
 
 function update(dt)
-
     for j=0,3 do
         for b=0, 10 do
             if gengine.input.joypads[j]:isJustDown(b) then
                 print("Joypad" .. j .. " button" .. b .. " is just pressed!")
             end
         end
+    end
+
+    for b=1,3 do
+        if gengine.input.mouse:isJustDown(b) then
+            print("Mouse button " .. b .. " is just pressed!")
+        end
+    end
+
+    for k=1,100 do
+        if gengine.input.keyboard:isJustDown(k) then
+            print("Keyboard key " .. k .. " is just pressed!")
+        end
+    end
+
+    local x, y = gengine.input.mouse:getPosition()
+
+    if lastMousePosition.x ~= x or lastMousePosition.y ~= y then
+        lastMousePosition:set(x, y)
+        print("Mouse position is now " .. tostring(lastMousePosition))
     end
 
     if gengine.input.keyboard:isJustUp(41) then
