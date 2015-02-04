38162b5edf7b2ab355edfeefe05a0ea529f1b9e2
Gauthier Billot <gauthier.billot@fishingcactus.com>
2015-02-04 13:31:30 +0100

    updated particle sample

--- samples/02-particles/main.lua
+++ samples/02-particles/main.lua
@@ -1,36 +1,34 @@
 function init()
     gengine.application.setName("[gengine-samples] 02-particles")
-    gengine.application.setExtent(512, 512)
+    gengine.application.setExtent(800, 600)
 end
-
-local entity
 
 function start()
     gengine.graphics.setClearColor(0,0.1,0.1,1)
 
     gengine.graphics.texture.create("particle.png")
 
-    entity = gengine.entity.create()
+    local e = gengine.entity.create()
 
-    entity:addComponent(
+    e:addComponent(
         ComponentParticleSystem(),
         {
             texture = gengine.graphics.texture.get("particle"),
-            size = 1000,
-            rate = 133,
+            size = 10000,
+            rate = 1000,
             emitterLifeTime = 1000,
-            extentRange = {vector2(64,64), vector2(128,128)},
-            lifeTimeRange = {1, 2},
+            extentRange = {vector2(32,32), vector2(34,34)},
+            lifeTimeRange = {0.5, 1},
             directionRange = {0, 2*3.14},
             speedRange = {100, 500},
             rotationRange = {0, 0},
-            spinRange = {0, 10},
+            spinRange = {-10, 10},
             scales = {vector2(1, 1)},
-            colors = {vector4(1,1,1,1), vector4(1,1,1,0)}
+            colors = {vector4(0.8,0.8,0.9,1), vector4(0.3,0.3,0.9,1), vector4(0,0,0,0)}
         }
         )
 
-    entity:insert()
+    e:insert()
 end
 
 function update(dt)
