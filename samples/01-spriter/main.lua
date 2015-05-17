ef9166614c63e7ee1c22f157ab1708ac66819a75
Gauthier Billot <gauthier.billot@fishingcactus.com>
2015-05-17 21:45:53 +0200

    Added Spriter sample

---
+++ samples/01-spriter/main.lua
@@ -0,0 +1,23 @@
+function init()
+    gengine.application.setName("[gengine-samples] 01-spriter")
+    gengine.application.setExtent(320,200)
+end
+
+local characterEntity
+local leftAnimation, rightAnimation, loopingAnimation
+
+function start()
+    gengine.graphics.setClearColor(0,0.1,0.1,1)
+
+    gengine.graphics.spriter.loadFile("example.scon")
+end
+
+function update(dt)
+    if gengine.input.keyboard:isJustUp(41) then
+        gengine.application.quit()
+    end
+end
+
+function stop()
+
+end
