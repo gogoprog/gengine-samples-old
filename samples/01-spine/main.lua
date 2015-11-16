b664cec528fe6bb9391303a690809a0f86208a30
Gauthier Billot <gauthier.billot@fishingcactus.com>
2015-11-16 11:39:57 +0100

    Updated Spine sample

--- samples/01-spine/main.lua
+++ samples/01-spine/main.lua
@@ -3,28 +3,56 @@ function init()
     gengine.application.setExtent(800,600)
 end
 
-local e
+local boy, raptor
 
 function start()
     gengine.graphics.setClearColor(0,0.1,0.1,1)
     gengine.graphics.texture.createFromDirectory("data/")
+    gengine.graphics.spine.create("data/spineboy.json")
     gengine.graphics.spine.create("data/raptor.json")
 
-    e = gengine.entity.create()
+    boy = gengine.entity.create()
 
-    e:addComponent(
+    boy:addComponent(
         ComponentSpine(),
         {
-            animation = gengine.graphics.spine.get("raptor-walk"),
             layer = 0
-        }
+        },
+        "spine"
         )
 
-    e.position:set(0, -500)
-    e:insert()
+    boy.scale:set(0.4, 0.4)
+    boy.position:set(-300, -300)
+    boy:insert()
+
+    boy.spine:setAnimation(gengine.graphics.spine.get("spineboy-walk"), 0, true)
+
+    raptor = gengine.entity.create()
+
+    raptor:addComponent(
+        ComponentSpine(),
+        {
+            layer = 0
+        },
+        "spine"
+        )
+
+    raptor.scale:set(0.4, 0.4)
+    raptor.position:set(100, -300)
+    raptor:insert()
+
+    raptor.spine:setAnimation(gengine.graphics.spine.get("raptor-walk"), 0, true)
 end
 
 function update(dt)
+    if gengine.input.mouse:isJustDown(1) then
+        boy.spine:setAnimation(gengine.graphics.spine.get("spineboy-shoot"), 0, false, 0)
+        boy.spine:addAnimation(gengine.graphics.spine.get("spineboy-run"), 0, true, 0)
+    end
+    if gengine.input.mouse:isJustDown(3) then
+        boy.spine:setAnimation(gengine.graphics.spine.get("spineboy-jump"), 0, false, 0)
+        boy.spine:addAnimation(gengine.graphics.spine.get("spineboy-walk"), 0, true, 0)
+    end
     if gengine.input.keyboard:isJustUp(41) then
         gengine.application.quit()
     end
