f48c26d759013cc118f6a98ea58989c5b7c5ec52
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-02-21 23:13:43 +0100

    wip

--- samples/01-sprite/main.lua
+++ samples/01-sprite/main.lua
@@ -1,25 +1,26 @@
 function init()
     gengine.application.setName("[gengine-samples] 01-sprite")
     gengine.application.setExtent(320,200)
+    gengine.application.setFullscreen(false)
 end
 
 local logoEntity
 
 function start()
-    gengine.graphics.setClearColor(0,0.1,0.1,1)
+    --gengine.graphics.setClearColor(0,0.1,0.1,1)
 
-    gengine.graphics.texture.create("logo.png")
+    --gengine.graphics.texture.create("logo.png")
 
     logoEntity = gengine.entity.create()
 
-    logoEntity:addComponent(
+    --[[logoEntity:addComponent(
         ComponentSprite(),
         {
-            texture = gengine.graphics.texture.get("logo"),
+            --texture = gengine.graphics.texture.get("logo"),
             extent = vector2(256, 128),
             layer = 0
         }
-        )
+        )]]
 
     logoEntity:insert()
 end
@@ -29,10 +30,9 @@ local total = 0
 function update(dt)
     total = total + dt
     logoEntity.position.x = math.sin(total * 2) * 50
-
-    if gengine.input.keyboard:isJustUp(41) then
+    --[[if gengine.input.keyboard:isJustUp(41) then
         gengine.application.quit()
-    end
+    end]]
 end
 
 function stop()
