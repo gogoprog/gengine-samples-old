9a1b7b5cfffc6b2faac1574a020bae1bde6110ea
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-03-01 20:54:43 +0100

    Updated animated sprite sample

--- samples/01-animated_sprite_2d/main.lua
+++ samples/01-animated_sprite_2d/main.lua
@@ -4,17 +4,32 @@ function init()
     gengine.application.setFullscreen(false)
 end
 
-local player
+local player1, player2
 
 function start()
     gengine.graphics.setClearColor(Color(0,0,0.1));
 
-    local e = gengine.entity.create()
+    player1 = gengine.entity.create()
 
-    e:addComponent(
+    player1:addComponent(
         ComponentAnimatedSprite2D(),
         {
-            --animationSet = cache:GetResource("AnimationSet2D", "spriter/player.scml"),
+            animationSet = cache:GetResource("AnimationSet2D", "spriter/player.scml"),
+            animation = "walk",
+            layer = 0
+        },
+        "sprite"
+        )
+
+    player1.position.x = -200
+    player1.position.y = -200
+    player1:insert()
+
+    player2 = gengine.entity.create()
+
+    player2:addComponent(
+        ComponentAnimatedSprite2D(),
+        {
             animationSet = cache:GetResource("AnimationSet2D", "spine/spineboy.json"),
             animation = "walk",
             layer = 0
@@ -22,31 +37,37 @@ function start()
         "sprite"
         )
 
-    e.position.y = -200
-    e:insert()
+    player2.scale = Vector3(-0.5, 0.5, 1)
+    player2.position.y = -200
+    player2.position.x = 200
+    player2:insert()
 
-    player = e
 end
 
 function update(dt)
     if gengine.input.isKeyDown(30) then
-        player.sprite.animation = "crouch_down"
+        player1.sprite.animation = "crouch_down"
+        player2.sprite.animation = "shoot"
     end
 
     if gengine.input.isKeyDown(31) then
-        player.sprite.animation = "stand_up"
+        player1.sprite.animation = "stand_up"
+        player2.sprite.animation = "run"
     end
 
     if gengine.input.isKeyDown(32) then
-        player.sprite.animation = "crouch_idle"
+        player1.sprite.animation = "crouch_idle"
+        player2.sprite.animation = "jump"
     end
 
     if gengine.input.isKeyDown(33) then
-        player.sprite.animation = "jump_start"
+        player1.sprite.animation = "jump_start"
+        player2.sprite.animation = "death"
     end
 
     if gengine.input.isKeyDown(34) then
-        player.sprite.animation = "fall_start"
+        player1.sprite.animation = "fall_start"
+        player2.sprite.animation = "idle"
     end
 
     if gengine.input.isKeyDown(41) then
