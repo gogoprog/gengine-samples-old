c6ad01b704041a037a166b7ab8e5aa1b8b254018
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-03-02 21:47:05 +0100

    Wip on tile sample

--- samples/50-tiled/main.lua
+++ samples/50-tiled/main.lua
@@ -23,19 +23,43 @@ function init()
 end
 
 function start()
-    gengine.graphics.setClearColor(0.5,0.5,0.5,1)
-    gengine.physics.createWorlds(1)
-    gengine.physics:getWorld(1):setGravity(vector2(0,-100))
+    gengine.graphics.setClearColor(Color(0.5,0.5,0.5,1))
 
-    local entities = gengine.tiled.createEntities("data/map.lua", vector2(-192, -192))
+    local e
 
-    for k, e in ipairs(entities) do
-        e:insert()
+    e = gengine.entity.create()
+
+    e:addComponent(
+        ComponentTileMap2D(),
+        {
+            --tmxFile = cache:GetResource("TmxFile2D", "isometric_grass_and_water.tmx")
+            tmxFile = cache:GetResource("TmxFile2D", "map.tmx")
+        },
+        "tileMap"
+        )
+
+    e.position.x = -192
+    e.position.y = -192
+    e:insert()
+
+    for l=0, e.tileMap.numLayers-1 do
+        local layer = e.tileMap:GetLayer(l)
+        for x=0,layer.width - 1 do
+            for y=0,layer.height - 1 do
+                local tile = layer:GetTile(x, y)
+
+                if tile then
+                    if tile:HasProperty("physic") or layer:HasProperty("physic") then
+                        print(":TODO:")
+                    end
+                end
+            end
+        end
     end
 end
 
 function update(dt)
-    if gengine.input.keyboard:isJustUp(41) then
+    if gengine.input.isKeyJustDown(41) then
         gengine.application.quit()
     end
 end
